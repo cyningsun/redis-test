5f569ddef1ed902e466756fb828b8e3df8b10d3c
yinhang.sun <yinhang.sun@shopee.com>
2023-11-17 15:01:02 +0800

    correct errors

--- slotcount/main.go
+++ slotcount/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"sort"
+	"strings"
 	"time"
 
 	"github.com/go-redis/redis/v8"
@@ -23,7 +24,7 @@ func main() {
 	})
 
 	// 创建上下文
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Millisecond)
+	ctx, cancel := context.WithTimeout(context.Background(), 3000*time.Millisecond)
 	defer cancel()
 
 	// 调用 CLUSTER SLOTS 命令
@@ -32,15 +33,40 @@ func main() {
 		log.Fatal(err)
 	}
 
-	sort.Slice(slots, func(i, j int) bool {
-		return slots[i].Start < slots[j].Start
+	// group by node id
+	nodeSlotsMap := make(map[string][]redis.ClusterSlot)
+	for _, slot := range slots {
+		nodeSlotsMap[slot.Nodes[0].ID] = append(nodeSlotsMap[slot.Nodes[0].ID], slot)
+	}
+
+	// nodeSlots map to slice
+	nodeSlots := make([][]redis.ClusterSlot, 0, len(nodeSlotsMap))
+	for _, slots := range nodeSlotsMap {
+		nodeSlots = append(nodeSlots, slots)
+	}
+
+	// sort  slots by slot range in node
+	for _, slots := range nodeSlots {
+		sort.Slice(slots, func(i, j int) bool {
+			return slots[i].Start < slots[j].Start
+		})
+	}
+
+	// sort node by slot range in node
+	sort.Slice(nodeSlots, func(i, j int) bool {
+		return nodeSlots[i][0].Start < nodeSlots[j][0].Start
 	})
 
-	// 计算每个分片的 SLOTS 数量
-	fmt.Println("slot_range\tnode_id\tnode_addr\tslot_count")
-	for _, slotRange := range slots {
-		slotsCount := slotRange.End - slotRange.Start + 1
+	// calc slot count by node id
+	fmt.Println("node_id\tnode_addr\tslot_count\tslot_range")
+	for _, slots := range nodeSlots {
+		var slotCount int
+		slotRanges := make([]string, 0, len(slots))
+		for _, slot := range slots {
+			slotCount += int(slot.End - slot.Start + 1)
+			slotRanges = append(slotRanges, fmt.Sprintf("[%d-%d]", slot.Start, slot.End))
+		}
 
-		fmt.Printf("%d-%d\t%s\t%s\t%d\n", slotRange.Start, slotRange.End, slotRange.Nodes[0].ID, slotRange.Nodes[0].Addr, slotsCount)
+		fmt.Printf("%s\t%s\t%d\t%s\n", slots[0].Nodes[0].ID, slots[0].Nodes[0].Addr, slotCount, strings.Join(slotRanges, " "))
 	}
 }
